fc110b93c9f25ab1557a5923c52c1126f2c46ede
VanArya <neohrju2313@gmail.com>
2022-05-08 12:17:44 +0430

    trolls

--- main.py
+++ main.py
@@ -1,43 +1,5 @@
-import mymodule as mym
-
-mym.greeting("sen")
-mym.zarb(17)
-x = mym.P1["age"]
-print(x)
-
-import datetime
-
-x = datetime.datetime.now()
-print(x.year)
-
-import math
-
-f = math.sqrt(64)
-print(int(f))
-
 import re
+def disemvowel(string_):
+    print(re.sub(r"[auieoO]","", string_))
 
-x = "naser very GAV"
-y = re.search("alil", x)
-if y:
-    print("HA")
-else:
-    print("NA")
-
-import re
-
-x = "ali very GAV and Sag"
-y = re.findall("a", x)
-print(y)
-
-import re
-
-x = "ali very GAV and Sag"
-y = re.sub("a", "", x, 3)
-print(y)
-
-import camelcase
-
-c = camelcase.CamelCase()
-x = "ali az sag kamtar ast"
-print(c.hump(x))
+#link = https://www.codewars.com/kata/52fba66badcd10859f00097e/train/python
