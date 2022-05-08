366eeca8749befba704a1ea5ba70fed4d8c8564c
Seyyed Naser Bahador <=>
2022-05-08 12:50:12 +0430

    -Added more vowels

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import re
 def disemvowel(string_):
-    print(re.sub(r"[auieoO]","", string_))
+    print(re.sub(r"[aAuUiIeEoO]","", string_))
 
+print(disemvowel("ThE qUicK bRoWn fOx JuMpA ovEr ThE lAzY Dog"))
 #link = https://www.codewars.com/kata/52fba66badcd10859f00097e/train/python
