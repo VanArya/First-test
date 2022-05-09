b71fae0f52f7f72de67bfa2b8c4fa25fd8b22db3
Seyyed Naser Bahador <=>
2022-05-09 09:03:28 +0430

    -type(string) is always String, you need to convert it to int or float -if string is not int you will get exception

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 def isDigit(string):
-    if type(string) is int or float:
-        return True
-#     if type(string) is str:
-#          return False
-#     if type(string) is float:
-#         return True
-    else:
-        return False
+    try:
+        if type(int(string)) is int:
+            return True
+    except:
+        try:
+            if type(float(string)) is float:
+                return True
+        except:
+            return False
 
+print(isDigit("--12.5"))
  #link https://www.codewars.com/kata/57126304cdbf63c6770012bd/train/python
