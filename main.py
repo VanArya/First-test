b9dfcd2a49dda7cf79651ada66d19127d83a93a1
Seyyed Naser Bahador <=>
2022-05-10 18:46:40 +0430

    -implemented my conbin

--- main.py
+++ main.py
@@ -3,7 +3,14 @@ def Conbin(n):
     c = str(a.count('1'))
     return c
 
-
-print(Conbin(0))
-print(Conbin(4))
+def myConBin(n):
+    total = 0
+    while( n > 0 ):
+        if n % 2 == 1:
+            total += 1
+        n = n // 2
+    return total
+    
+for i in range(0,16):
+    print(Conbin(i), myConBin(i))
 #https://www.codewars.com/kata/526571aae218b8ee490006f4/train/python
