aee85d7f284cc3f5b3b14de18a30eb361444d572
VanArya <neohrju2313@gmail.com>
2022-05-10 18:29:33 +0430

    count bin

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
-class py_solution:
-    def roman_to_int(self, s):
-        rom_val = {'I': 1, 'IV' : 4, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        int_val = 0
-        for i in range(len(s)):
-            if i > 0 and rom_val[s[i]] > rom_val[s[i - 1]]:
-                int_val += rom_val[s[i]] - 2 * rom_val[s[i - 1]]
-            else:
-                int_val += rom_val[s[i]]
-        return int_val
+def Conbin(n):
+    a = str(bin(n))
+    c = str(a.count('1'))
+    return c
 
-print(py_solution().roman_to_int('MMMCMLXXXVI'))
-# https://www.codewars.com/kata/51b6249c4612257ac0000005/train/python
+
+print(Conbin(0))
+print(Conbin(4))
